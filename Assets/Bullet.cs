642d150914cb3c69ea45136216ebc582bd99c048
GliptisV <kiziridis@outlook.com>
2019-01-23 21:39:06 +0200

    lesson-3

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10, _lifetime = 4;
+    [SerializeField] private int _damage = 1;
+
+    private void Start()
+    {
+        Destroy(gameObject, _lifetime);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.right * _speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Health hp = collision.gameObject.GetComponent<Health>();                    
+            hp.TakeDamage(_damage);
+            Destroy(gameObject);
+       }          
+    }
+
+}
+
